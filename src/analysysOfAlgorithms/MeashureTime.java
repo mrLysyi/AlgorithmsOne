5fd6de27c58a856484a029693ac608f042331b66
mrChe <cheburano@gmail.com>
2014-09-30 21:24:07 +0300

    add src

---
+++ src/analysysOfAlgorithms/MeashureTime.java
@@ -0,0 +1,12 @@
+package analysysOfAlgorithms;
+
+import com.algorithms.*;
+//import algs4.Bag.*;
+
+public class MeashureTime {
+
+	public static void main(String[] args) {
+		Bag<?> bag = new Bag();
+
+	}
+}
