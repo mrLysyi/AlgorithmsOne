d1a5fba12e8e87483111bb871cd1183f65fde3ae
mrLysyi <cheburano@gmail.com>
2014-10-27 17:41:17 +0200

    50&

--- src/weekThree/Brute.java
+++ src/weekThree/Brute.java
@@ -5,29 +5,31 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.TreeSet;
 
 import com.algorithms.In;
 import com.algorithms.StdDraw;
 import com.algorithms.StdOut;
 
 public class Brute {
-	private static String filename = "/home/kokoko/workspace/Algorithms_sendfiles/input56.txt";
+	private static String filename = "/home/hellow/workspaceJava/Algorithms_testFiles/collinear/input8.txt";
 
 	public static void main(String[] args) throws IOException {
-		// rescale coordinates and turn on animation mode
-		StdDraw.setXscale(0, 32768);
+		StdDraw.setXscale(0, 32768); // rescale coordinates and turn on
+										// animation mode
 		StdDraw.setYscale(0, 32768);
 		StdDraw.show(0);
 		StdDraw.setPenRadius(0.01); // make the points a bit larger
 
 		// read from the input
 		// String filename = args[0];
-
 		In in = new In(filename);
 		int N = in.readInt();
-		int countL = countLines(filename);
-		Point[] parr = new Point[countL];
+		// int countL = countLines(filename);
+		int countL = N;
 
+		Point[] parr = new Point[countL];
 		for (int i = 0; i < N; i++) { // Create array of Points
 			int x = in.readInt();
 			int y = in.readInt();
@@ -35,62 +37,68 @@ public class Brute {
 			parr[i] = p;
 			p.draw();
 		}
-		// Point zeroPoint = new Point(0, 0);
-		// Arrays.
-		Arrays.sort(parr);
-		// Iterator<Point> iterator = arr.iterator();
-		Point to = new Point(0, 0);
-		int count;
-		double outerSloope, innerSloope;
-		for (int i = 0; i < countL; i++) { // OUTER LOOP
-			count = 0;
-			Point p1 = parr[i];
-			// outerSloope = .slopeTo(p1);
-			// sloopeArr[i] = outerSloope;
-			for (int j = i + 1; j < countL; j++) { // OUTER_INNER LOOP
-			// count = 0;
-				Point p2 = parr[j];
-				outerSloope = p1.slopeTo(p2);
-				if (count >= 5)
-					break;
-				for (int k = j; k < countL; k++) { // INNER_InnerLoop
-					if (count >= 5)
-						break;
-					Point p3 = parr[k];
-					innerSloope = p1.slopeTo(p3);
-					if (innerSloope == outerSloope && count <= 5) {
-						// System.out.println("5");
+		int count = 0;
+		Arrays.sort(parr, 0, N-1);
+		Point pA, p1 = new Point(0, 0);
+		Point pB, p2 = new Point(0, 0);
+		Point pC, p3 = new Point(0, 0);
+		Point pD, p4 = new Point(0, 0);
+		// Point to = new Point(0, 0);
+		boolean print = false;
+		for (int a = 0; a < countL; a++) {		
+			
+			for (int b = a + 1; b < N; b++) {
+				pA = parr[a];
+				pB = parr[b];
+				for (int c = b + 1; c < N; c++) {
+					pC = parr[c];
+					if (pA.slopeTo(pB) == pA.slopeTo(pC))
+//						if (pB.slopeTo(pC) == pB.slopeTo(pA)) 
+						{	
+							for (int d = c + 1; d < N; d++) {
+								pD = parr[d];
+//								if (pD.slopeTo(pA) == pD.slopeTo(pB))
+									if (pD.slopeTo(pC) == pD.slopeTo(pB))
+//									if (pD.slopeTo(pC)!=pD.slopeTo(parr[d+1]))
+									{
+										p1 = pA;
+										p2 = pB;
+										p3 = pC;
+										p4 = pD;
+										// if (p4.compareTo(pD) != 1){ //p4
+										// < pD
+										// p4 = pD;
+										//
+										// }
+										// StdOut.print( pD+ "\n");
+//										 if (d == countL-1) {
+										p1.drawTo(p4);
+										count++;
+										print = true;
+										StdOut.println(p1 + " -> " + p2 + " -> " + p3
+												+ " -> " + p4);
+										print = false;
+										
+//										 p4 = to; // set 0
+//										 }
 
-						to = p3;
-						if (count == 0)
-							StdOut.print(to);
-						else
-							StdOut.print(" -> " + to);
-						count++;
-
-					}
-
+									}
+							}
+						}
 				}
-
 			}
-			if (count >= 4) {
-				p1.drawTo(to);
-				StdOut.println();
-				// break;
-				// System.out.println("6");
-			}
-
 		}
+
 		// MY TEST
-
-		// for (Point s : parr){
-		// System.out.println(s.toString());
+		// int count = 0;
+		// for (Point s : parr) {
+		// System.out.println(count + " : " + s.toString());
+		// count++;
 		// }
-
 		StdDraw.show(0);
-
 		// reset the pen radius
 		StdDraw.setPenRadius();
+		 System.out.println(count);
 	}
 
 	private static int countLines(String filename) throws IOException {
