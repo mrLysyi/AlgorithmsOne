45dccbb015fd46b62254f86faefdc420b977d3f4
mrLysyi <cheburano@gmail.com>
2014-10-29 17:22:15 +0200

    week3 almost gone

--- src/weekThree/Fast.java
+++ src/weekThree/Fast.java
@@ -5,13 +5,30 @@ package weekThree;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeSet;
 
 import com.algorithms.In;
 import com.algorithms.StdDraw;
+import com.algorithms.StdOut;
 
 public class Fast {
-	private static String filename = "/home/kokoko/workspace/Algorithms_sendfiles/Colinear_ex/input56.txt";
+
+	private static boolean onLine(Point p1, Point p2, Point p3) {
+		return p1.SLOPE_ORDER.compare(p2, p3) == 0;
+	}
+
+	private static boolean onLine(HashSet<Point> set, Point p1) {
+		// Point p2;
+		// Point p3;
+		TreeSet<Point> pSet = new TreeSet<Point>(set);
+		Point[] druck = (pSet.toArray(new Point[pSet.size()]));
+		return p1.SLOPE_ORDER.compare(pSet.first(), pSet.last()) == 0;
+		// return false;
+	}
+
+	private static String filename = "/home/hellow/workspaceJava/Algorithms_testFiles/collinear/input56.txt";
 
 	public static void main(String[] args) {
 		// read from the input
@@ -26,11 +43,9 @@ public class Fast {
 
 		int N = in.readInt();
 
-		// int countL = N;
-
 		Point[] parr = new Point[N];
+		boolean[] pointOnLine = new boolean[N];
 		Double[] slopeArr = new Double[N];
-		TreeSet<Point> pSet = new TreeSet<Point>();
 
 		for (int i = 0; i < N; i++) { // Create array of Points
 			int x = in.readInt();
@@ -38,32 +53,30 @@ public class Fast {
 			Point p = new Point(x, y);
 			parr[i] = p;
 			p.draw();
+			pointOnLine[i] = false;
 		}
 
 		int count = 0;
-
+		int drawCount = 0;
 		Point p1 = new Point(0, 0);
-		// Point pB, p2 = new Point(0, 0);
-		// Point pC, p3 = new Point(34560, 60);
-		// Point pD, p4 = new Point(457, 2456);
+		Point pB, p2 = new Point(23452345, 234523452);
+		Point pC, p3 = new Point(34560, 60);
+		Point pD, p4 = new Point(457, 2456);
 		Point q1, q2 = new Point(0, 0);
 
-		// boolean print = false;
-		count = 0;
-		int drawCount = 0;
+		HashMap<Double, HashSet<Point>> hashmap = new HashMap<Double, HashSet<Point>>();
 
-		double previous, next;
-		for (int a = 0; a < N; a++) { // main upper loop
-			System.out
-					.println("____________________NEW Iteration____________________________");
-			count = 0; // Initialize for inner loop
-			previous = 0.0;
+		// HashSet<Point> temPoint = new HashSet<Point>();
 
+		Arrays.sort(parr, 0, N);
+		double slope = 0.011111111111111111111;
+		for (int a = 0; a < N; a++) { 							// main upper loop
+			
 			Arrays.sort(parr, 0, N); // Sort
 			p1 = parr[a];
-			Arrays.sort(parr, p1.SLOPE_ORDER);
+//			Arrays.sort(parr, p1.SLOPE_ORDER);
 			// pSet.clear();
-			for (int b = 0; b < N; b++) { // FiLL slope Array
+			for (int b = 0; b < N-1; b += 2) { 					// FiLL slope Array
 				if (b == N - 1) {
 					q1 = parr[b];
 					q2 = p1;
@@ -71,50 +84,85 @@ public class Fast {
 					q1 = parr[b];
 					q2 = parr[b + 1];
 				}
-
-				if (p1.SLOPE_ORDER.compare(q1, q2) == 0) { // Search
-															// slope.points
-					slopeArr[b] = p1.slopeTo(parr[b]);
+				// q1 = parr[b];
+				// q2 = parr[b + 1];
+				slope = p2.slopeTo(q2);
+//				
+				if (p1.SLOPE_ORDER.compare(q1, q2) == 0  ) { // Search     && q1.SLOPE_ORDER.compare(p1, q2) == 0
+					pointOnLine[a] = true;
+					
+//					double slope = p1.slopeTo(q2)+a; // slope.points
+					// slopeArr[b] = slope;
 					// System.out.println("№:" + b + " slopeArr[b]: "
 					// + slopeArr[b]+ ", p1: " + p1);
-					pSet.add(parr[b]);
-					count++;
+					// pSet.add(parr[b]);
 
-				}
-
-				else {
-					if (count >= 2) {
-						p1.drawTo(pSet.last()); // Draw
-						System.out.println(p1);
-						// p1.draw();
-						drawCount++;
-
+					// System.out.println(slope);
+					if (hashmap.containsKey(slope)) { // update
+						System.out.println("slope: " + slope +" a= "+ a +" b= "+b);
+						HashSet<Point> temPoint = new HashSet<Point>();
+						// System.out.print("update slope...");
+						temPoint = hashmap.get(slope); // get
+						// System.out.println("get kee"+hashmap.get(slope));
+						if (onLine(temPoint, q1))
+							temPoint.add(q1); // update
+						if (onLine(temPoint, q2))
+							temPoint.add(q2);
+						if (onLine(temPoint, p1) )
+							temPoint.add(p1); // update
+						hashmap.put(slope, temPoint); // putIn
+						// System.out.println("new walue is: " +
+						// hashmap.toString());
+						//
+					} else { // put first time  								CREATE LINe	!!!
+//						slope = p1.slopeTo(q2); 
+						HashSet<Point> pointDraw = new HashSet<Point>();
+						pointDraw.add(q1); // put point in set
+						pointDraw.add(q2);
+						count++;
+						pointDraw.add(p1);
+						// System.out.println("create slope "+ slope + " point "
+						// + pointDraw);
+						
+						hashmap.put(slope, pointDraw);
 					}
-
-					pSet.clear();
-					count = 0;
+					// count++;
+					// System.out.println("loop1 walue is: " +
 				}
+				// System.out.println("loop2 walue is: " + hashmap.toString());
 
-				// slopeArr[b] = null;
+				// System.out.println("loop3 walue is: " + hashmap.toString());
 			}
+			// System.out.println("loop4 walue is: " + hashmap.toString());
+		}
+		// }
+		System.out.println(hashmap.size());
+		System.out.println(hashmap.toString());
+		System.out.println("--------------------");
+		// draw and print
+		HashSet<Point> draw = new HashSet<Point>();
+		
+		for (Map.Entry<Double, HashSet<Point>> entry : hashmap.entrySet()) {		// PRINT && DRAW
+			draw = entry.getValue();
+			TreeSet<Point> pSet = new TreeSet<Point>(entry.getValue());
+//			System.out.println(draw);
+			if (pSet.size() > 3) {
+				pSet.first().drawTo(pSet.last());
+				Point[] druck = new Point[pSet.size()];
 
-			// for (int d = 0; d < N - 1; d++) { // check
-			//
-			// if (slopeArr[d] != null) {
-			// next = slopeArr[d];
-			// if (previous == next ) {
-			// pSet.add(parr[d]);
-			// count++;
-			// } else {
-			// if (count > 1) {
-			// p1.drawTo(pSet.last());
-			// }
-			// pSet.clear();
-			// count = 0;
-			// }
-			// previous = next;
-			// }
-			// }
+				druck = (pSet.toArray(new Point[pSet.size()]));
+				for (int i = 0; i < pSet.size(); i += 1) {
+					if (i == pSet.size() - 1)
+						StdOut.print(druck[i]+" "+entry.getKey());
+					else
+						StdOut.print(druck[i] + " -> ");
+				}
+				StdOut.println("");
+				// StdOut.println(pSet.first() + " -> " + pSet.last() + " -> " +
+				// pC + " -> " + pD);
+			}
+			// System.out.println(draw.toArray(a));
+
 		}
 
 		StdDraw.show(0);
@@ -138,10 +186,24 @@ public class Fast {
 		// // set.
 		// System.out.println(set.toString());
 		// System.out.println(p3.slopeTo(p4));
-		System.out.println("drCount: " + drawCount);
-		HashMap<Double, HashSet<Point>> sss = new HashMap<Double, HashSet<Point>>();
-		
+		// System.out.println("drCount: " + drawCount);
+		// HashMap<Double, HashSet<Point>> sss = new HashMap<Double,
+		// HashSet<Point>>();
+		// HashSet<Point> hp1= new HashSet<Point>();
+		// HashSet<Point> hp2= new HashSet<Point>();
+		// HashSet<Point> hp3= new HashSet<Point>();
+		// hp1.add(p1);
+		//
+		//
+		// hp2.add(p3);
+		// hp2.add(p4);
+		// hp2.add(p3);
+		//
+		// sss.put(1.0, hp2);
+		// sss.put(1.0, hp1);
+		//
 
+		// System.out.println(sss.values());
 		// I used a nested java collection. HashMap<Double, HashSet<Point>>.
 		// Before any iteration, sort points by it's lexical order. In
 		// iteration, HashMap could judge if a key in it by the complexity O(1).
