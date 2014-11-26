005ddc324191f23d3481498e88e5fb6ea2656889
mrLysyi <cheburano@gmail.com>
2014-11-26 17:19:30 +0200

    week5_last_finish=(

--- src/week5/KdTree.java
+++ src/week5/KdTree.java
@@ -13,20 +13,35 @@ public class KdTree {
 	private static class Node {
 		private Point2D p; // the point
 		private RectHV rect; // the axis-aligned rectangle corresponding to this
-							// node
+								// node
 		private Node lb; // the left/bottom subtree
 		private Node rt; // the right/top subtree
 
 		Node(Point2D point) {
 			this.p = point;
-//			this.rect = new RectHV(0.0, point.x(), 0.0, point.y());
+			Node lb = null;
+			Node rt = null;
+
+			// this.rect = new RectHV(0.0, point.x(), 0.0, point.y());
 			// this.lb = leftBottom;
 			// this.rt = rightTop;
+		}
+
+		private int compareTo(Node that) {
+			if (this.p.x() < that.p.x())
+				return -1;
+			if (this.p.x() > that.p.x())
+				return +1;
+			if (this.p.y() < that.p.y())
+				return -1;
+			if (this.p.y() > that.p.y())
+				return +1;
+			return 0;
 		}
 	}
 
 	public KdTree() {
-		 first = new Node(null);
+		first = new Node(null);
 	} // construct an empty set of points
 
 	public boolean isEmpty() {
@@ -37,49 +52,49 @@ public class KdTree {
 		return count;
 	} // number of points in the set
 
-	public void insert(Point2D that) {
+	public void insert(Point2D thatP) {
 		Node current = first;
-//		Node previous = null;
+		Node previous = null;
+		Node next = null;
+		Node thisNode = new Node(thatP);
+		boolean left = false;
+		int cmp;
 		if (count == 0) {
-			this.first.p = that;
+			this.first.p = thatP;
 			count++;
-			return;		
+			System.out.println("insert frirst " + count);
+			return;
 		} else {
-			while (true) {
-//				previous = current;
-				if (that.compareTo(current.p) > 0) { // looking for last point
-					
-					if (current.lb == null){
-						current.lb = new Node(that);
-						System.out.println("insert left " + count);
-						count++;
-						return;
+			while (true) { // looking for last point
+				cmp = thisNode.compareTo(current);
+				if (cmp < 0) { // left
+					next = current.lb;
+					if (next != null && thisNode.compareTo(next) > 0) { // collizia
+						left = false;
+						next = current.rt;
 					} else
-					current = current.lb;
-				} else if (that.compareTo(current.p) < 0){
-					
-					if (current.rt == null){
-						current.rt = new Node(that);
-						System.out.println("insert right " + count);
-						count++;
-						return;
-					}
-					current = current.rt;
+						left = true;
+				} else if (cmp > 0) {// right
+
+					next = current.rt;
+					if (next != null && thisNode.compareTo(next) < 0) {
+						left = true;
+						next = current.lb;
+					} else
+						left = false;
 				}
-//				if (current == null) { // insert new point && rect
-//					if (that.compareTo(previous.p) > 0) {
-//						previous.lb = new Node(that);
-//						count++;
-//						return;
-//					} else {
-//						previous.rt = new Node(that);
-//						count++;
-//						return;
-//					}
-//				}
+				if (next == null) {
+					if (left)
+						current.lb = new Node(thatP);
+					else
+						current.rt = new Node(thatP);
+					count++;
+					return;
+				}
+				current = next;
 			}
 			// operation with current_previous
-		}		
+		}
 	} // add the point to the set (if it is not already in the set)
 
 	public boolean contains(Point2D that) {
@@ -105,23 +120,24 @@ public class KdTree {
 
 	public Iterable<Point2D> range(RectHV rect) {
 		Node test = first;
-//		while (test != null){
-//			test = rectRange(rectRange(test, rect), rect);
-//		}
+		// while (test != null){
+		// test = rectRange(rectRange(test, rect), rect);
+		// }
 		return this.range;
 	} // all points that are inside the rectangle
 
-//	private static Node rectRange(Node node, RectHV rect) {
-//		if (!rect.intersects(node.rect))
-//			return null;
-//			if(node.lb.rect.intersects(rect) && node.rt.rect.intersects(rect)){	// left
-//				return rectRange(node.lb,rect);		// if !=null  ???
-//			}
-//			else {					// right
-//				return node.rt;		// if !=null  ???
-//			}				
-////		return null;		
-//	}
+	// private static Node rectRange(Node node, RectHV rect) {
+	// if (!rect.intersects(node.rect))
+	// return null;
+	// if(node.lb.rect.intersects(rect) && node.rt.rect.intersects(rect)){ //
+	// left
+	// return rectRange(node.lb,rect); // if !=null ???
+	// }
+	// else { // right
+	// return node.rt; // if !=null ???
+	// }
+	// // return null;
+	// }
 
 	public Point2D nearest(Point2D p) {
 		if (isEmpty())
@@ -133,15 +149,22 @@ public class KdTree {
 		KdTree test = new KdTree();
 		Point2D point;
 		System.out.println(test.isEmpty());
-		test.insert(new Point2D(0.7,0.2));
-		test.insert(new Point2D(0.5,0.4));	//left
-		test.insert(new Point2D(0.2,0.3));	//left
-		test.insert(new Point2D(0.4,0.7));	//left
-		test.insert(new Point2D(0.9,0.6));	//right
+		// Point2D zeroNine = new Point2D(0.9,0.6);
+
+		test.insert(new Point2D(0.7, 0.2));
+		// System.out.println(zeroNine.compareTo(test.first.p));
+		test.insert(new Point2D(0.9, 0.6)); // right
+		test.insert(new Point2D(0.5, 0.4)); // left
+		test.insert(new Point2D(0.2, 0.3)); // left
+		test.insert(new Point2D(0.4, 0.7)); // left
+
 		System.out.println(test.first.p + " -first");
 		System.out.println(test.first.lb.p);
-//		System.out.println(test.first.rt.p);
-//		System.out.println((test.first.lb).lb.p);
-//		System.out.println(test.first.lb.rt.p);
+		System.out.println(test.first.rt.p); // null
+
+		System.out.println((test.first.lb).lb.p);
+		// System.out.println((test.first.lb).lb.rt.p); //wrong
+		System.out.println((test.first.lb).rt.p);
+
 	} // unit testing of the methods (optional)
 }
